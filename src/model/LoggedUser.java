3de152572d7a306d270f3efb263a5024b1aa4234
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-11-26 20:11:24 +0700

    refactor(LoggedUser): Renamed LoggedUser#getInstance(user) to LoggedUser#initialize(user)

--- src/model/LoggedUser.java
+++ src/model/LoggedUser.java
@@ -3,36 +3,39 @@ package model;
 import helper.ImageManager;
 import javafx.scene.image.Image;
 
-import java.io.InputStream;
 import java.sql.Blob;
 
-public class LoggedUser extends User{
+public class LoggedUser extends User {
 
     private static LoggedUser loggedUser;
     private Image profileImage;
 
-    public static LoggedUser getInstance(User user) {
-        if(loggedUser == null) {
-            loggedUser = new LoggedUser(user.getId(), user.getUsername(), user.getEmail(), user.getPassword(), user.getDob(), user.getBlobProfile());
-        }
-        return loggedUser;
-    }
-
-    public static LoggedUser getInstance() {
-        return loggedUser;
-    }
-
     private LoggedUser(String id, String username, String email, String password, String dob, Blob profile) {
         super(id, username, email, password, dob, profile);
 
-        if(profile != null){
+        if (profile != null) {
             this.profileImage = ImageManager.convertBlobImage(profile);
         }
+    }
+
+    public static void initialize(User user) {
+        if (loggedUser != null) {
+            return;
+        }
+
+        loggedUser = new LoggedUser(user.getId(), user.getUsername(), user.getEmail(), user.getPassword(), user.getDob(), user.getBlobProfile());
+    }
+
+    public static LoggedUser getInstance() {
+        assert loggedUser != null;
+
+        return loggedUser;
     }
 
     public Image getProfileImage() {
         return profileImage;
     }
+
     public void setProfileImage(Image image) {
         this.profileImage = image;
     }
