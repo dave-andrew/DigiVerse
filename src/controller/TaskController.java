59ab3ef57ec3f7efa0ae77b61af2e005d236d4ab
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-11-28 11:29:08 +0700

    refactor(TaskController): Made TaskController#createTask void

--- src/controller/TaskController.java
+++ src/controller/TaskController.java
@@ -1,8 +1,6 @@
 package controller;
 
 import database.TaskQuery;
-import helper.StageManager;
-import helper.Toast;
 import model.LoggedUser;
 import model.Task;
 
@@ -16,15 +14,15 @@ public class TaskController {
         this.taskQuery = new TaskQuery();
     }
 
-    public Task createTask(String title, String description, String deadlineAt, boolean scored, String classid) {
+    public void createTask(String title, String description, String deadlineAt, boolean scored, String classid) {
 
         if(title.isEmpty() || description.isEmpty() || deadlineAt.isEmpty()) {
-            return null;
+            return;
         }
 
         Task newTask = new Task(LoggedUser.getInstance().getId(), LoggedUser.getInstance(), title, description, deadlineAt, scored);
 
-        return this.taskQuery.createTask(newTask, classid);
+        this.taskQuery.createTask(newTask, classid);
     }
 
     public ArrayList<Task> getClassroomTask(String classid) {
