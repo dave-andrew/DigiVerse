62f823e406c75fcfa1bd0661d460b30807f1e206
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-11-30 16:17:25 +0700

    feat: CRUD test on MainTest

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -6,18 +6,22 @@ import helper.Closer;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class MainTest {
 
     public MainTest() {
-        this.query();
+        this.select();
+        this.insert();
+        this.update();
+        this.delete();
     }
 
     public static void main(String[] args) {
         new MainTest();
     }
 
-    private void query() {
+    private void select() {
         try (Closer closer = new Closer()) {
             Results results = closer.add(new NeoQueryBuilder(NeoQueryBuilder.QueryType.SELECT)
                     .table("msuser")
@@ -29,6 +33,47 @@ public class MainTest {
             while (set.next()) {
                 System.out.println(set.getString("UserID"));
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insert() {
+        try (Closer closer = new Closer()) {
+            closer.add(new NeoQueryBuilder(NeoQueryBuilder.QueryType.INSERT)
+                    .table("msuser")
+                    .values("UserID", UUID.randomUUID().toString())
+                    .values("UserName", "Tester123")
+                    .values("UserEmail", "email@123.com")
+                    .values("UserPassword", "hello1234")
+                    .values("UserDOB", "1990-01-01")
+                    .getResults());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void update() {
+        try (Closer closer = new Closer()) {
+            closer.add(new NeoQueryBuilder(NeoQueryBuilder.QueryType.UPDATE)
+                    .table("msuser")
+                    .values("UserName", "Tester456")
+                    .condition("UserName", "=", "Tester123")
+                    .getResults());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void delete() {
+        try (Closer closer = new Closer()) {
+            closer.add(new NeoQueryBuilder(NeoQueryBuilder.QueryType.DELETE)
+                    .table("msuser")
+                    .condition("UserName", "=", "Tester456")
+                    .getResults());
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
