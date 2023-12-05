a410cfcc6216288c025b3fe98f96f036bd2bbbbb
Dep <109551168+dave-andrew@users.noreply.github.com>
2023-12-05 14:48:14 +0700

    feat: starting banner

--- src/main/java/net/slc/dv/view/classroom/list/ClassroomListView.java
+++ src/main/java/net/slc/dv/view/classroom/list/ClassroomListView.java
@@ -1,12 +1,14 @@
 package net.slc.dv.view.classroom.list;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
+import net.slc.dv.builder.LabelBuilder;
 import net.slc.dv.controller.ClassController;
 import net.slc.dv.controller.MemberController;
 import javafx.geometry.Insets;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import net.slc.dv.model.Classroom;
+import net.slc.dv.model.LoggedUser;
 import net.slc.dv.view.classroom.detail.ClassroomDetailView;
 import net.slc.dv.view.classroom.list.component.ClassCard;
 import net.slc.dv.view.home.Home;
@@ -26,6 +28,13 @@ public class ClassroomListView extends GridPane {
         init();
 
         ArrayList<Classroom> classroomList = classController.getUserClassroom();
+
+        if(classroomList.isEmpty()) {
+            HBox hBox = startingBanner();
+
+            mainPane.getChildren().add(hBox);
+            return;
+        }
 
         int columnCount = 0;
         int maxColumns = 4;
@@ -65,5 +74,55 @@ public class ClassroomListView extends GridPane {
         classController = new ClassController();
     }
 
+    private HBox startingBanner() {
+        HBox hBox = new HBox();
 
+        BorderPane borderPane = new BorderPane();
+        borderPane.setPadding(new Insets(30));
+
+        VBox messageContainer = new VBox();
+        messageContainer.setAlignment(Pos.CENTER_LEFT);
+        messageContainer.setSpacing(20);
+
+        Label dear = LabelBuilder.create("Dear " + LoggedUser.getInstance().getUsername() + ",")
+                .setStyleClass("title")
+                .build();
+
+        Label p1 = new Label("Welcome to an exciting journey of learning and discovery of DigiVerse! I am thrilled to have each and every one of you in our class. This promises to be a rewarding and enriching experience, filled with opportunities for growth, collaboration, and success.");
+        p1.setWrapText(true);
+
+        Label p2 = new Label("In this application, we value curiosity, creativity, and the pursuit of knowledge. Together, we will explore new concepts, engage in meaningful discussions, and support one another on our academic endeavors.");
+        p2.setWrapText(true);
+
+        Label p3 = new Label("We look forward to getting to know each of you and learning with you this year.");
+        p3.setWrapText(true);
+
+        Label p4 = new Label("Let's make this a fantastic academic journey together!");
+        p4.setWrapText(true);
+
+        VBox pContainer = new VBox();
+
+        Label p5 = new Label("Sincerely,");
+        Label p6 = new Label("DigiVerse Team");
+        p6.setStyle("-fx-font-size: 16px");
+
+        pContainer.getChildren().addAll(p5, p6);
+        pContainer.setAlignment(Pos.CENTER_RIGHT);
+
+        messageContainer.getChildren().addAll(dear, p1, p2, p3, p4, pContainer);
+
+        messageContainer.setPrefWidth(600);
+        messageContainer.setPrefHeight(500);
+        messageContainer.setAlignment(Pos.CENTER_LEFT);
+        messageContainer.getStyleClass().add("card");
+
+        VBox messageBox = new VBox();
+        messageBox.setAlignment(Pos.CENTER);
+        messageBox.getChildren().add(messageContainer);
+
+        hBox.getChildren().add(messageBox);
+        hBox.setAlignment(Pos.CENTER);
+
+        return hBox;
+    }
 }
